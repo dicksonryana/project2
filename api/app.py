7a415bc479479fc6c47950214b2285ccdb4eb473
devinmilligan <DCMilligan88@gmail.com>
2020-04-16 13:26:45 -0700

    apiwork

--- api/app.py
+++ api/app.py
@@ -16,7 +16,8 @@ app = Flask(__name__)
 def welcome():
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/guns2019"
+        f"/api/v1.0/guns2019<br/>"
+        f"/api/v1.0/guns2020<br/>"
     )
 
 @app.route("/api/v1.0/guns2019")
@@ -39,3 +40,24 @@ def guns2019():
     guns2019['ar15'] = guns2019ar15
 
     return jsonify(guns2019)
+
+@app.route("/api/v1.0/guns2020")
+def guns2020():
+    result = engine.execute("select * from guns2020")
+    guns2020 = {}
+    guns2020date = []
+    guns2020ammunition = []
+    guns2020stores = []
+    guns2020ar15 = []
+    for row in result:
+        guns2020date.append(row[1])
+        guns2020ammunition.append(row[2])
+        guns2020stores.append(row[3])
+        guns2020ar15.append(row[4])
+    result.close()
+    guns2020['dates'] = guns2020date
+    guns2020['ammunition'] = guns2020ammunition
+    guns2020['stores'] = guns2020stores
+    guns2020['ar15'] = guns2020ar15
+
+    return jsonify(guns2020)

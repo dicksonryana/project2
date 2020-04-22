0dc803c578855f47929626400f0c5e157330ae6e
philip-morlier <58234998+philip-morlier@users.noreply.github.com>
2020-04-22 05:03:14 -0700

    more tweet js

---
+++ layoutit/src/js/tweet_buttons.js
@@ -0,0 +1,11 @@
+function pageRedirectnra() {
+   window.location.href = "tweethtml/nra.html";
+}; 
+
+function pageRedirectcdc() {
+   window.location.href = "tweethtml/cdc.html";
+}; 
+
+function pageRedirectflix() {
+   window.location.href = "tweethtml/flix.html";
+}; 
